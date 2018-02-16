04a6988a90269c7d495da42f42fe7e1062b7b2b3
J. Rafael Rodríguez Galván <rafael.rodriguez@uca.es>
2018-02-16 08:57:43 +0100

    Keller-segel tests are ok. Introduced pot argument processing

--- examples/keller_segel/keller_segel_ex1/keller_segel_ex1.C
+++ examples/keller_segel/keller_segel_ex1/keller_segel_ex1.C
@@ -17,9 +17,9 @@
 
 
 
-// <h1>Transient Example 1 - Solving a Transient Linear System in Parallel</h1>
+// <h1>Keller-Segel Example 1 - Solving a Keller-Segel Linear System in Parallel</h1>
 // \author Rafa Rodríguez Galván
-// \date 2003
+// \date 2018
 //
 // This example solves a Keller-Segel system.  The initial condition
 // is given, and the solution is advanced in time with a standard
@@ -41,11 +41,14 @@
 #include "libmesh/fe.h"
 #include "libmesh/quadrature_gauss.h"
 #include "libmesh/dof_map.h"
+#include "libmesh/fe_base.h"
+#include "libmesh/string_to_enum.h"
 #include "libmesh/sparse_matrix.h"
 #include "libmesh/numeric_vector.h"
 #include "libmesh/dense_matrix.h"
 #include "libmesh/dense_vector.h"
 #include "libmesh/exodusII_io.h"
+#include "libmesh/getpot.h"
 
 // This example will solve a linear transient system,
 // so we need to include the TransientLinearImplicitSystem definition.
@@ -126,6 +129,19 @@ int main (int argc, char ** argv)
   // Skip this 2D example if libMesh was compiled as 1D-only.
   libmesh_example_requires(2 <= LIBMESH_DIM, "2D support");
 
+  // Parse the input file
+  GetPot infile("keller_segel_ex1.in");
+  infile.parse_command_line(argc, argv);
+
+  // Read in parameters from the input file
+  const unsigned int n_mesh_intervals = infile("n_mesh_intervals", 20);
+  const Real dt              = infile("delta_t", 0.0001);
+  const unsigned int n_time_steps   = infile("n_time_steps", 5);
+  const unsigned int save_n_steps   = infile("save_n_steps", 10);
+  const unsigned int fe_order   = infile("fe_order", 1);
+  std::string fe_family_str = infile("fe_family", std::string("LAGRANGE"));
+  FEFamily fe_family = Utility::string_to_enum<FEFamily>(fe_family_str);
+
   // Read the mesh from file.  This is the coarse mesh that will be used
   // in example 10 to demonstrate adaptive mesh refinement.  Here we will
   // simply read it in and uniformly refine it 5 times before we compute
@@ -135,11 +151,12 @@ int main (int argc, char ** argv)
   // distributed across the default MPI communicator.
   Mesh mesh(init.comm());
 
+  // MeshTools::Generation::build_cube (mesh,
   MeshTools::Generation::build_square (mesh,
-                                       200, 200,
+                                       n_mesh_intervals, n_mesh_intervals,
                                        -2., 2.,
                                        -2., 2.,
-                                       TRI3);
+                                       QUAD9);
 
   // mesh.read ("mesh.xda");
 
@@ -167,14 +184,15 @@ int main (int argc, char ** argv)
     equation_systems.add_system<TransientLinearImplicitSystem> ("Keller-Segel.v");
 
   // Adds the variable "u" to "Keller-Segel.u", using first-order approximation.
-  system_u.add_variable ("u", FIRST);
+  system_u.add_variable ("u", static_cast<Order>(fe_order), fe_family);
 
   // Adds the variable "v" to "Keller-Segel.v", using first-order approximation.
-  system_v.add_variable ("v", FIRST);
+  system_v.add_variable ("v", static_cast<Order>(fe_order), fe_family);
 
   // Give the system a pointer to the matrix assembly for u,v
   // and initialization functions.
   system_u.attach_assemble_function (assemble_ks_u);
+  system_v.attach_assemble_function (assemble_ks_v);
   system_u.attach_init_function (init_ks_u);
   system_v.attach_init_function (init_ks_v);
 
@@ -186,9 +204,9 @@ int main (int argc, char ** argv)
 
   // Write out the initial conditions.
 #ifdef LIBMESH_HAVE_EXODUS_API
-  // If Exodus is available, we'll write all timesteps to the same file
+  // If Exodus is available, we'll write all time_steps to the same file
   // rather than one file per timestep.
-  std::string exodus_filename = "transient_ex1.e";
+  std::string exodus_filename = "keller_segel_ex1.e";
   ExodusII_IO(mesh).write_equation_systems (exodus_filename, equation_systems);
 #else
   GMVIO(mesh).write_equation_systems ("out_000.gmv", equation_systems);
@@ -208,14 +226,15 @@ int main (int argc, char ** argv)
   // looping over the specified time interval and calling the
   // solve() member at each time step.  This will assemble the
   // system and call the linear solver.
-  const Real dt = 1.e-4; //0.025;
   system_u.time = 0.;
+  system_v.time = 0.;
 
-  for (unsigned int t_step = 0; t_step < 50; t_step++)
+  for (unsigned int t_step = 0; t_step < n_time_steps; t_step++)
     {
       // Incremenet the time counter, set the time and the
       // time step size as parameters in the EquationSystem.
       system_u.time += dt;
+      system_v.time += dt;
 
       equation_systems.parameters.set<Real> ("time") = system_u.time;
       equation_systems.parameters.set<Real> ("dt")   = dt;
@@ -232,14 +251,22 @@ int main (int argc, char ** argv)
             << t_step
             << ", time="
             << std::fixed
-            << std::setw(6)
-            << std::setprecision(3)
+            << std::setw(9)
+            << std::setprecision(6)
             << std::setfill('0')
             << std::left
-            << system_u.time
+            << system_u.time << " (" << system_v.time << ")"
+
             <<  "...";
 
-        libMesh::out << out.str() << std::endl;
+        out << std::scientific;
+
+	out << std::endl << "  max(u): " << system_u.solution->max();
+	out << std::endl << "  min(u): " << system_u.solution->min();
+	out << std::endl << "  max(v): " << system_v.solution->max();
+	out << std::endl << "  min(v): " << system_v.solution->min();
+
+	libMesh::out << out.str() << std::endl;
       }
 
       // At this point we need to update the old solution vector.  The
@@ -252,11 +279,13 @@ int main (int argc, char ** argv)
       *system_u.old_local_solution = *system_u.current_local_solution;
       *system_v.old_local_solution = *system_v.current_local_solution;
 
-      // Assemble & solve the linear system
+      // Assemble & solve the linear system for u
       equation_systems.get_system("Keller-Segel.u").solve();
+      // Assemble & solve the linear system for v
+      equation_systems.get_system("Keller-Segel.v").solve();
 
-      // Output evey 10 timesteps to file.
-      if ((t_step+1)%10 == 0)
+      // Output evey nsteps time_steps to file.
+      if ((t_step+1)%save_n_steps == 0)
         {
 
 #ifdef LIBMESH_HAVE_EXODUS_API
@@ -481,9 +510,9 @@ void assemble_ks_u (EquationSystems & es,
 		{
 		  Ke(i,j) += JxW[qp]*(
                                       // Mass-matrix
-                                      phi[i][qp]*phi[j][qp] +
+                                      phi[i][qp]*phi[j][qp]
 				      // Diffusion term
-				      dt*c_u0 * (dphi[i][qp]*dphi[j][qp])
+				      - dt*c_u0 * (dphi[i][qp]*dphi[j][qp])
                                       );
                 }
 	      // The RHS contribution
@@ -491,50 +520,11 @@ void assemble_ks_u (EquationSystems & es,
 				// Mass matrix term
                                 u_old*phi[i][qp] +
 				// Convection term
-				-dt * c_u1*u_old*(grad_v_old*dphi[i][qp])
+				- dt * c_u1*u_old*(grad_v_old*dphi[i][qp])
                                 );
 
             }
         }
-
-      // // At this point the interior element integration has
-      // // been completed.  However, we have not yet addressed
-      // // boundary conditions.  For this example we will only
-      // // consider simple Dirichlet boundary conditions imposed
-      // // via the penalty method.
-      // //
-      // // The following loops over the sides of the element.
-      // // If the element has no neighbor on a side then that
-      // // side MUST live on a boundary of the domain.
-      // {
-      //   // The penalty value.
-      //   const Real penalty = 1.e10;
-
-      //   // The following loops over the sides of the element.
-      //   // If the element has no neighbor on a side then that
-      //   // side MUST live on a boundary of the domain.
-      //   for (unsigned int s=0; s<elem->n_sides(); s++)
-      //     if (elem->neighbor_ptr(s) == libmesh_nullptr)
-      //       {
-      //         fe_face->reinit(elem, s);
-
-      //         for (unsigned int qp=0; qp<qface.n_points(); qp++)
-      //           {
-      //             const Number value = exact_solution (qface_points[qp](0),
-      //                                                  qface_points[qp](1),
-      //                                                  system.time);
-
-      //             // RHS contribution
-      //             for (std::size_t i=0; i<psi.size(); i++)
-      //               Fe(i) += penalty*JxW_face[qp]*value*psi[i][qp];
-
-      //             // Matrix contribution
-      //             for (std::size_t i=0; i<psi.size(); i++)
-      //               for (std::size_t j=0; j<psi.size(); j++)
-      //                 Ke(i,j) += penalty*JxW_face[qp]*psi[i][qp]*psi[j][qp];
-      //           }
-      //       }
-      // }
 
       // If this assembly program were to be used on an adaptive mesh,
       // we would have to apply any hanging node constraint equations
@@ -548,6 +538,196 @@ void assemble_ks_u (EquationSystems & es,
       system.rhs->add_vector    (Fe, dof_indices);
     }
 
-  // That concludes the system matrix assembly routine.
+  // That concludes the system matrix assembly routine for u
+#endif // #ifdef LIBMESH_ENABLE_AMR
+}
+
+// Now we define the assemble function for v
+void assemble_ks_v (EquationSystems & es,
+		    const std::string & libmesh_dbg_var(system_name))
+{
+#ifdef LIBMESH_ENABLE_AMR
+  // It is a good idea to make sure we are assembling
+  // the proper system.
+  libmesh_assert_equal_to (system_name, "Keller-Segel.v");
+
+  // Get a constant reference to the mesh object.
+  const MeshBase & mesh = es.get_mesh();
+
+  // The dimension that we are running
+  const unsigned int dim = mesh.mesh_dimension();
+
+  // Get a reference to the Keller-Segel.u system object.
+  TransientLinearImplicitSystem & system =
+    es.get_system<TransientLinearImplicitSystem> ("Keller-Segel.v");
+
+  // Get a reference to the Keller-Segel.v system object.
+  TransientLinearImplicitSystem & system_u =
+    es.get_system<TransientLinearImplicitSystem> ("Keller-Segel.u");
+
+  // Get a constant reference to the Finite Element type for v (the first
+  // (and only) variable in the system).
+  FEType fe_type = system.variable_type(0);
+
+  // Get a constant reference to the Finite Element type for u and
+  // assert it is equal to FE type for u
+  FEType fe_type_u = system_u.variable_type(0);
+  assert(fe_type_u == fe_type);
+
+  // Build a Finite Element object of the specified type.  Since the
+  // FEBase::build() member dynamically creates memory we will
+  // store the object as a UniquePtr<FEBase>.  This can be thought
+  // of as a pointer that will clean up after itself.
+  UniquePtr<FEBase> fe      (FEBase::build(dim, fe_type));
+  UniquePtr<FEBase> fe_face (FEBase::build(dim, fe_type));
+
+  // A Gauss quadrature rule for numerical integration.
+  // Let the FEType object decide what order rule is appropriate.
+  QGauss qrule (dim,   fe_type.default_quadrature_order());
+  QGauss qface (dim-1, fe_type.default_quadrature_order());
+
+  // Tell the finite element object to use our quadrature rule.
+  fe->attach_quadrature_rule      (&qrule);
+  fe_face->attach_quadrature_rule (&qface);
+
+  // Here we define some references to cell-specific data that
+  // will be used to assemble the linear system.  We will start
+  // with the element Jacobian * quadrature weight at each integration point.
+  const std::vector<Real>& JxW      = fe->get_JxW();
+
+  // The element shape functions evaluated at the quadrature points.
+  const std::vector<std::vector<Real>>& phi = fe->get_phi();
+
+  // The element shape function gradients evaluated at the quadrature
+  // points.
+  const std::vector<std::vector<RealGradient>>& dphi = fe->get_dphi();
+
+  // A reference to the DofMap object for this system. The DofMap
+  // object handles the index translation from node and element
+  // numbers to degree of freedom numbers.
+  const DofMap & dof_map = system.get_dof_map();
+
+  // Define data structures to contain the element matrix and
+  // right-hand-side vector contribution.  Following basic finite
+  // element terminology we will denote these "Ke" and "Fe".
+  DenseMatrix<Number> Ke;
+  DenseVector<Number> Fe;
+
+  // This vector will hold the degree of freedom indices for the
+  // element. These define where in the global system the element
+  // degrees of freedom get mapped.
+  std::vector<dof_id_type> dof_indices;
+
+  // Extract parameters
+  const Real dt = es.parameters.get<Real> ("dt");
+  const Real c_v0 = es.parameters.get<Real> ("c_v0");
+  const Real c_v1 = es.parameters.get<Real> ("c_v1");
+  const Real c_v2 = es.parameters.get<Real> ("c_v2");
+
+  // Now we will loop over all the elements in the mesh that live on
+  // the local processor. We will compute the element matrix and
+  // right-hand-side contribution.  Since the mesh will be refined we
+  // want to only consider the ACTIVE elements, hence we use a variant
+  // of the active_elem_iterator.
+  MeshBase::const_element_iterator       el     = mesh.active_local_elements_begin();
+  const MeshBase::const_element_iterator end_el = mesh.active_local_elements_end();
+
+  for ( ; el != end_el; ++el)
+    {
+      // Store a pointer to the element we are currently
+      // working on.  This allows for nicer syntax later.
+      const Elem * elem = *el;
+
+      // Get the degree of freedom indices for the current element.
+      // These define where in the global matrix and right-hand-side
+      // this element will contribute to.
+      dof_map.dof_indices (elem, dof_indices);
+
+      // Compute the element-specific data for the current element.
+      // This involves computing the location of the quadrature points
+      // (q_point) and the shape functions (phi, dphi) for the current
+      // element.
+      fe->reinit (elem);
+
+      // Zero the element matrix and right-hand side before
+      // summing them.  We use the resize member here because
+      // the number of degrees of freedom might have changed from
+      // the last element.  Note that this will be the case if the
+      // element type is different (i.e. the last element was a
+      // triangle, now we are on a quadrilateral).
+      Ke.resize (dof_indices.size(),
+                 dof_indices.size());
+
+      Fe.resize (dof_indices.size());
+
+      // Now we will build the element matrix and right-hand-side.
+      // Constructing the RHS requires the solution and its
+      // gradient from the previous timestep.  This myst be
+      // calculated at each quadrature point by summing the
+      // solution degree-of-freedom values by the appropriate
+      // weight functions.
+      for (unsigned int qp=0; qp<qrule.n_points(); qp++)
+        {
+          // Values to hold the old solution & its gradient.
+          Number u_old = 0.;
+          Gradient grad_u_old;
+	  // Previous values of v & its gradient
+	  Number v_old = 0.;
+	  Gradient grad_v_old = 0.;
+
+          // Compute the old solution & its gradient.
+          for (std::size_t l=0; l<phi.size(); l++)
+            {
+              u_old += phi[l][qp]*system_u.old_solution(dof_indices[l]);
+              v_old += phi[l][qp]*system.old_solution(dof_indices[l]);
+
+              // This will work,
+              // grad_u_old += dphi[l][qp]*system.old_solution(dof_indices[l]);
+              // but we can do it without creating a temporary like this:
+              grad_u_old.add_scaled (dphi[l][qp], system_u.old_solution(dof_indices[l]));
+              grad_v_old.add_scaled (dphi[l][qp], system.old_solution(dof_indices[l]));
+	      // std::cout << system_v.old_solution(dof_indices[l]) << std::endl;
+            }
+
+          // Now compute the element matrix and RHS contributions, for each local dof.
+          for (std::size_t i=0; i<phi.size(); i++)
+            {
+	      // The matrix contribution
+	      for (std::size_t j=0; j<phi.size(); j++)
+		{
+		  Ke(i,j) += JxW[qp]*(
+                                      // Time derivative (mass-matrix)
+                                      phi[i][qp]*phi[j][qp] +
+				      // Diffusion term (stiffness matrix)
+				      dt*c_v0 * (dphi[i][qp]*dphi[j][qp]) +
+				      // Reaction term (mass matrix)
+                                      dt*c_v1 * phi[i][qp]*phi[j][qp]
+                                      );
+                }
+	      // The RHS contribution
+	      Fe(i) += JxW[qp]*(
+				// Time derivative (mass matrix) term
+                                v_old*phi[i][qp] +
+				// Coupling term with u (live cell density)
+				dt*c_v2 * u_old*phi[i][qp]
+                                );
+
+            }
+        }
+
+      // If this assembly program were to be used on an adaptive mesh,
+      // we would have to apply any hanging node constraint equations
+      dof_map.constrain_element_matrix_and_vector (Ke, Fe, dof_indices);
+
+      // The element matrix and right-hand-side are now built
+      // for this element.  Add them to the global matrix and
+      // right-hand-side vector.  The SparseMatrix::add_matrix()
+      // and NumericVector::add_vector() members do this for us.
+      system.matrix->add_matrix (Ke, dof_indices);
+      system.rhs->add_vector    (Fe, dof_indices);
+    }
+
+  // That concludes the system matrix assembly routine for v
+
 #endif // #ifdef LIBMESH_ENABLE_AMR
 }
